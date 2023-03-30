f6e0b8daa1fc1b5d6e895d4ef60498e40c76e6eb
Hudson Menezes <hudson.bm23@gmail.com>
2023-03-30 17:57:20 -0300

    Header, Nav and NavMobile done

--- src/components/Program.js
+++ src/components/Program.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+
+// import icons
+import PhoneIcon from "../assets/img/phone.svg";
 
 const Program = () => {
-  return <div>Program</div>;
+  return (
+    <div>
+      {/* phone icon and phone number */}
+      <div className="flex lg:gap-x-[10px] items-baseline justify-end">
+        <img
+          className="w-[28px] h-[28px] lg:w-[34px] lg:h-[34px]"
+          src={PhoneIcon}
+          alt="Phone Icon"
+        />
+        <div className="lg:text-[23px] text-blue font-extrabold">
+          + 55 21 912345678
+        </div>
+      </div>
+      {/* open hours */}
+      <div className="text-sm">
+        {/* show only in desktop mode */}
+        <div className="hidden lg:flex">
+          Funcionamento: Seg - Sab: 10am - 6pm
+        </div>
+        {/* show only in mobile mode */}
+        <div className="lg:hidden text-right">Seg - Sab: 10am - 6pm</div>
+      </div>
+    </div>
+  );
 };
 
 export default Program;
